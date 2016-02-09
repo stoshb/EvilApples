4a181182571192240664e4dc75dee7b1b0201bb9
Stan Banes <Stan@stans-mbp.fios-router.home>
2016-02-09 13:03:20 -0600

    Initial Commit

---
+++ EvilApples/Classes/AppDelegate.h
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.h
+//
+//  Created by : Stan Banes
+//  Project    : EvilApples
+//  Date       : 2/9/16
+//
+//  Copyright (c) 2016 Stan Banes.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+// -----------------------------------------------------------------------
+
+@interface AppDelegate : CCAppDelegate
+
+// -----------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ EvilApples/Classes/HelloWorldScene.h
@@ -0,0 +1,61 @@
+//
+//  HelloWorldScene.h
+//
+//  Created by : Stan Banes
+//  Project    : EvilApples
+//  Date       : 2/9/16
+//
+//  Copyright (c) 2016 Stan Banes.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+@interface HelloWorldScene : CCScene
+
+// -----------------------------------------------------------------------
+
+- (instancetype)init;
+
+// -----------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
